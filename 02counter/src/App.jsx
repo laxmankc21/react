47626123d3c2ed4da6f7c64d024c57b5262df493
Laxman Kc <laxman2019kc@gmail.com>
2025-01-27 00:26:01 +0530

    Updated login feature

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+  // let counter = 15;
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(counter + 1);
+    console.log("clicked", counter);
+  };
+  const removeValue = () => {
+    setCounter(counter - 1);
+    console.log("clicked", counter);
+  };
+
+  return (
+    <>
+      <h1>Chair aur react</h1>
+      <h2>Counter Value:{counter}</h2>
+      <button onClick={addValue}>Add Value{counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value{counter}</button>
+      <p>footer:{counter}</p>
+    </>
+  );
+}
+
+export default App;
